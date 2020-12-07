f6ae0ff6c097ece9618953bc835e2a26344c2f40
Luke Chapman <Luke.Chapman@ccr.net>
2020-12-06 21:50:38 -0700

    Initiale file creations

---
+++ pokemon.js
@@ -0,0 +1,10 @@
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
